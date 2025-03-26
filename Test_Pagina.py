75f907dc9a8acdc96fcbfadffc5042bf4063bdf9
Belén Reyes <joia.belen.reyes@gmail.com>
2025-03-25 18:56:52 -0600

    Update Test_Pagina.py

--- Test_Pagina.py
+++ Test_Pagina.py
@@ -81,7 +81,7 @@ def load_css():
 # Cargar estilos
 load_css()
 # Insertar logo
-st.image("https://raw.githubusercontent.com/JoIA-25/JoIA/3bb4a51bc82be2d2b46b61d5322ff1c4a3fe90b8/logo.jpeg", use_container_width=False)
+st.image("https://joia-25.github.io/JoIA/partials/belen_animated_output.svg", use_container_width=False)
 
 # --------------------------
 # Funciones Speckle
